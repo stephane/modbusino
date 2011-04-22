d9b593ca89e853175e4ca012fdd3b29453b5524a
Stéphane Raimbault <stephane.raimbault@gmail.com>
2011-04-22 22:29:35 +0200

    Check the slave argument

--- Modbusino.cpp
+++ Modbusino.cpp
@@ -63,7 +63,9 @@ static uint16_t crc16(uint8_t *req, uint8_t req_length)
 }
 
 ModbusinoSlave::ModbusinoSlave(uint8_t slave) {
-    _slave = slave;
+    if (slave >= 0 & slave <= 247) {
+	_slave = slave;
+    }
 }
 
 void ModbusinoSlave::setup(long baud) {
